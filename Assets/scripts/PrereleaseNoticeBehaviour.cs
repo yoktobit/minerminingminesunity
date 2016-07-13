0f56f03fcecb094fea3787bf8c9284a2ef661b7a
Martin Windolph <martin@yoktobit.de>
2016-07-13 22:57:41 +0200

    fix #35, add a localization package

---
+++ Assets/scripts/PrereleaseNoticeBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PrereleaseNoticeBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void HandleClick()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+}
